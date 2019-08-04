541f95e4acb0850cebff7873477cf4aa78aa11c2
赵丰 (Zhao Feng) <616545598@qq.com>
2019-08-04 22:25:13 +0800

    Create bootstrap.ps1

---
+++ bootstrap.ps1
@@ -0,0 +1,5 @@
+Write-Output "Downloading Python 3.7"
+$url = "https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Windows-x86_64.exe"
+$file = "miniconda.exe"
+$downloader = new-object System.Net.WebClient
+$downloader.DownloadFile($url, $file)
